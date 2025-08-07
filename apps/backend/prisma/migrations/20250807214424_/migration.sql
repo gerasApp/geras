edcb6d5aac49f09db8a051bcaed790ea14f99a50
juanfra18 <juanpaoli@gmail.com>
2025-08-07 20:32:14 -0300

    Cambios en límites plan de retiro

---
+++ apps/backend/prisma/migrations/20250807214424_/migration.sql
@@ -0,0 +1,31 @@
+/*
+  Warnings:
+
+  - You are about to alter the column `initialAmount` on the `plan` table. The data in that column could be lost. The data in that column will be cast from `Int` to `BigInt`.
+  - You are about to alter the column `monthlyContribution` on the `plan` table. The data in that column could be lost. The data in that column will be cast from `Int` to `BigInt`.
+  - You are about to alter the column `objective` on the `plan` table. The data in that column could be lost. The data in that column will be cast from `Int` to `BigInt`.
+
+*/
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_plan" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "userId" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "code" TEXT NOT NULL,
+    "objective" BIGINT NOT NULL,
+    "type" TEXT NOT NULL,
+    "initialAmount" BIGINT NOT NULL,
+    "duration" INTEGER NOT NULL,
+    "monthlyContribution" BIGINT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "expectedReturnRate" REAL NOT NULL,
+    CONSTRAINT "plan_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+INSERT INTO "new_plan" ("code", "createdAt", "duration", "expectedReturnRate", "id", "initialAmount", "monthlyContribution", "name", "objective", "type", "userId") SELECT "code", "createdAt", "duration", "expectedReturnRate", "id", "initialAmount", "monthlyContribution", "name", "objective", "type", "userId" FROM "plan";
+DROP TABLE "plan";
+ALTER TABLE "new_plan" RENAME TO "plan";
+CREATE UNIQUE INDEX "plan_code_key" ON "plan"("code");
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
